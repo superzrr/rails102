8ee5ccdbc4a5ca56d0fc026cdb28c6c3e4efe22a
sueprzrr <superzrr@gmail.com>
2016-12-11 01:10:30 +0800

    empty groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,21 @@
+class GroupsController < ApplicationController
+	def index
+		@groups = Group.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
